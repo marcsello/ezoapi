0bb10a9ab59401b36d1c09213c7e093de7b93259
marcsello <punkosdmarcell@rocketmail.com>
2020-10-19 02:33:44 +0200

    Fixed getting backup basename

--- ezoapi/views/backups_view.py
+++ ezoapi/views/backups_view.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import glob
 import os
+import os.path
 
 from flask import abort, jsonify, current_app
 from flask_classful import FlaskView, route
@@ -12,15 +13,15 @@ class BackupsView(FlaskView):
     decorators = [apikey_required(False)]
 
     def index(self):
-        list_of_files = glob.glob(current_app.config["BACKUPS_FOLDER"])
-        return jsonify(list_of_files)
+        list_of_files = glob.glob(os.path.join(current_app.config["BACKUPS_FOLDER"], "*.tar.gz"))
+        return jsonify(os.path.basename(fname) for fname in list_of_files)  # empty lists handled by python properly
 
     @route("$latest")
     def get_latest(self):
-        list_of_files = glob.glob(current_app.config["BACKUPS_FOLDER"])
+        list_of_files = glob.glob(os.path.join(current_app.config["BACKUPS_FOLDER"], "*.tar.gz"))
 
         if not list_of_files:
             abort(404, "Backups folder empty")
 
         latest_file = max(list_of_files, key=os.path.getctime)
-        return jsonify(latest_file)
+        return jsonify(os.path.basename(latest_file))
