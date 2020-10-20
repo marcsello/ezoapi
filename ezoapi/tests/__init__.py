972797150e7f847bd9c071793a451616703fb380
marcsello <punkosdmarcell@rocketmail.com>
2020-10-20 22:57:53 +0200

    Added an example test

--- ezoapi/tests/__init__.py
+++ ezoapi/tests/__init__.py
@@ -1,1 +1,8 @@
 import os
+
+os.environ['LOCAL_BASIC_API_KEY'] = "asd"
+os.environ['LOCAL_PRIV_API_KEY'] = "asdasd"
+os.environ['BACKUPS_FOLDER'] = "asdasdasd"
+os.environ['RCON_HOST'] = "127.0.0.1"
+os.environ['RCON_PASSWORD'] = "asdasdasdasdasd"
+os.environ['QUERY_ADDRESS'] = "127.0.0.1"
